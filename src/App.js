5c53f25156f3058791ce785be17d9facc5bb1d89
Ece <ecekalpelli@gmail.com>
2023-04-30 22:48:37 +0300

    s6challenge

--- src/App.js
+++ src/App.js
@@ -1,18 +1,65 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import { setupWorker, rest } from 'msw'
+import Karakter from "./components/Karakter";
+import axios from "axios"
+import '../src/App.css';
+
 
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
+const [data, setData] = useState([]);
+const [loaded, setLoaded] = useState(false);
+
+
+const worker = setupWorker(
+  axios.get('https://github.com/octocat', (req, res, ctx) => {
+    return res(
+      ctx.delay(1500),
+      ctx.status(202, 'Mocked status'),
+      ctx.json({
+        message: 'Mocked response JSON body',
+      }),
+    )
+  }),
+)
+
+worker.start()
+
+// 2.yol
+// function fetchCharacters() {
+//   axios
+//   .get("https://swapi.dev/api/people/")
+//   .then((res) => {
+//    console.log("products:", res.data);
+//    setData(res.data);
+//    setLoaded(true);
+//   })
+//   .catch((err) => {
+//    setLoaded(false);
+//   }) 
+//   .finally(() => {
+//    console.log("final")
+//   });
+// }
+
+// useEffect(() => {
+//  fetchCharacters();
+// }, []);
+
+
+
 
   return (
+
     <div className="App">
-      <h1 className="Header">Karakterler</h1>
+      <h1 className="Header">REACT WARS</h1>
+      {!loaded && <p>Loading...<span role="img" aria-label='go!'>🚀</span>!</p>}
+      {loaded &&<Karakter characters={data} />}
+        
     </div>
   );
-}
 
+}
 export default App;
+
+
--- src/components/Karakter.js
+++ src/components/Karakter.js
@@ -1,1 +1,55 @@
 // Karakter bileşeniniz buraya gelecek
+import React from "react"
+import styled from 'styled-components';
+
+const Button = styled.button`
+  background-color: black;
+  color: white;
+  font-size: 20px;
+  padding: 10px 60px;
+  border-radius: 5px;
+  margin: 10px 0px;
+  cursor: pointer;
+`;
+
+
+const karakter = (data) => {
+
+    const { characters } = data;
+    
+
+    return(
+
+      <div>
+
+       {characters.map(character => {
+        return(
+        <div className="character-card">
+          <Button>`name: ${characters.name}`</Button>
+          <div className="character-children-card">
+          <p>`height: ${characters.height}`</p>
+          <p>`mass: ${characters.mass}`</p>
+          <p>`hair_color: ${characters.hair_color}`</p>
+          <p>`skin_color: ${characters.skin_color}`</p>
+          <p>`eye_color: ${characters.eye_color}`</p>
+          <p>`birth_year: ${characters.birth_year}`</p>
+          <p>`gender: ${characters.gender}`</p>
+          <p>`homeworld: ${characters.homeworld}`</p>
+          <p>`films: ${characters.films[0]}, ${characters.films[1]}, ${characters.films[2]} `</p>
+          <p>`species: ${characters.species}`</p>
+          <p>`films: ${characters.vehicles[0]}, ${characters.vehicles[1]}`</p>
+          <p>`starships: ${characters.starships[0]}, ${characters.starships[1]}`</p>
+          <p>`created: ${characters.created}`</p>
+          <p>`edited: ${characters.edited}`</p>
+          <p>`url: ${characters.url[0]}`</p>
+          </div> 
+        </div> 
+
+       )})};
+      
+      </div>
+ )};
+
+
+
+  export default karakter;
